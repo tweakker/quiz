d970cff3ef891ee66b112abed2905363319af82d
tweakker <tweakker@ya.ru>
2021-05-28 11:46:05 +0500

    push commit

---
+++ app/basic_entities/fillers.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+from typing import Dict, Optional, Any
+
+from core.text_preprocessing.preprocessing_result import TextPreprocessingResult
+from core.utils.exception_handlers import exc_handler
+
+from scenarios.scenario_models.field.field_filler_description import FieldFillerDescription
+from scenarios.user.user_model import User
+
+
+class CustomFieldFiller(FieldFillerDescription):
+
+    """
+        Тут можно создать собственные Fillers для использования их в заполнении полей форм и
+    """
+
+    def __init__(self, items: Optional[Dict[str, Any]], id: Optional[str] = None) -> None:
+        super(CustomFieldFiller, self).__init__(items, id)
+        items = items or {}
+        self.test_item = items.get("test_item")
+
+    @exc_handler(on_error_obj_method_name="on_extract_error")
+    def extract(self, text_preprocessing_result: TextPreprocessingResult, user: User, params) -> Optional[str]:
+        return None
+
+
+class QuizAnswerFiller(FieldFillerDescription):
+
+    def __init__(self, items: Optional[Dict[str, Any]], id: Optional[str] = None) -> None:
+        super().__init__(items, id)
+        items = items or {}
+        self.right_answers = items.get('right_answers')
+
+    @exc_handler(on_error_obj_method_name="on_extract_error")
+    def extract(self, text_preprocessing_result: TextPreprocessingResult, user: User, params) -> Optional[bool]:
+        return bool(text_preprocessing_result.words_tokenized_set.intersection(set(self.right_answers)))
---
+++ app/user/parametrizer.py
@@ -0,0 +1,18 @@
+from scenarios.user.parametrizer import Parametrizer
+
+
+class CustomParametrizer(Parametrizer):
+
+    """
+        Тут можно добавить новые данные, которые будут доступны для использования в ASL при использовании jinja
+    """
+
+    def __init__(self, user, items):
+        super(CustomParametrizer, self).__init__(user, items)
+
+    def _get_user_data(self, text_preprocessing_result=None):
+        data = super(CustomParametrizer, self)._get_user_data(text_preprocessing_result)
+        data.update({})
+        import pdb
+        pdb.set_trace()
+        return data
